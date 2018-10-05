917e082e71c91a577d49f62d386a813dc0f91e37
prajwol5667 <prajwol_shakya200@outlook.com>
2018-10-05 11:58:34 +0545

    test

--- run_docker.sh
+++ run_docker.sh
@@ -9,12 +9,6 @@ BUILD_NUM=$1
 echo "Running with BUILD_NUM=" $BUILD_NUM
 docker build -t prajwol5667/ci:$BUILD_NUM .
 
-if [  "$(docker ps -q -f name=t2)" ]; then
-    if [ "$(docker ps -aq -f status=running -f name=t2)" ]; then
-       echo "cleanup"
-        docker stop t2
-        docker rm t2
-    fi
-fi
+
 
 docker run -d --name t2  --rm -p 8011:8080 prajwol5667/ci:$BUILD_NUM
